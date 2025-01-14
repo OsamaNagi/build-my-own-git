06d9915f410ad720305c2e65113107ca8032bcdb
OsamaNagi <osamanagiworks@gmail.com>
2025-01-15 01:28:13 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -34,12 +34,7 @@ async function catFile(hash) {
 	// git cat-file -p 4b825dc642cb6eb9a060e54bf8d69288fbee4904
 
 	const file = await fs.readFileSync(path.join(process.cwd(), '.git', 'objects', hash.slice(0, 2), hash.slice(2)));
-	const unzipped = zlib.inflate(file, (err, buffer) => {
-		if (err) {
-			throw new Error(err);
-		}
-		return buffer;
-	});
+	const unzipped = zlib.inflateSync(file);
 	// format of the blob object is: blob <size>\0<content>
 	const result = unzipped.toString().split('\0')[1];
 	// result is the content of the file -> <content>
