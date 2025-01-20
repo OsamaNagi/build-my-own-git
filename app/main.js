bd6e4e2ea564ed5ce186ad2cdd0e7faf0eaf7337
OsamaNagi <osamanagiworks@gmail.com>
2025-01-20 18:50:18 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
+const crypto = require('crypto');
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.error('Logs from your program will appear here!');
-
-// Uncomment this block to pass the first stage
 const command = process.argv[2];
 
 switch (command) {
 	case 'init':
 		createGitDirectory();
 		break;
-	case 'cat-file':
+	case 'hash-object':
 		const hash = process.argv[4];
-		catFile(hash);
+		hashObject(hash);
+		break;
+	case 'cat-file':
+		const hashed = process.argv[4];
+		catFile(hashed);
 		break;
 	default:
 		throw new Error(`Unknown command ${command}`);
@@ -29,14 +30,36 @@ function createGitDirectory() {
 	console.log('Initialized git directory');
 }
 
-async function catFile(hash) {
+function hashObject(file) {
+	// git hash-object -w <file>
+	const content = fs.readFileSync(file);
+	// format of the blob object is: blob <size>\0<content>
+	const header = Buffer.from(`blob ${content.length}\0`);
+	// store the header and the content in a buffer
+	const store = Buffer.concat([header, content]);
+	// hash the store
+	const hash = crypto.createHash('sha1').update(store).digest('hex');
+	// compress the store
+	const compressed = zlib.deflateSync(store);
+	// store the compressed store in the .git/objects directory
+	const objectPath = path.join(process.cwd(), '.git', 'objects', hash.slice(0, 2));
+	// create the directory if it doesn't exist
+	fs.mkdirSync(objectPath, { recursive: true });
+	// write the file
+	fs.writeFileSync(path.join(objectPath, hash.slice(2)), compressed);
+	// print the hash
+	process.stdout.write(hash);
+}
+
+function catFile(hash) {
 	// .git/objects/firestTwoCharacters/remainingCharacters
 	// git cat-file -p 4b825dc642cb6eb9a060e54bf8d69288fbee4904
-
-	const file = await fs.readFileSync(path.join(process.cwd(), '.git', 'objects', hash.slice(0, 2), hash.slice(2)));
+	const file = fs.readFileSync(path.join(process.cwd(), '.git', 'objects', hash.slice(0, 2), hash.slice(2)));
 	const unzipped = zlib.inflateSync(file);
 	// format of the blob object is: blob <size>\0<content>
 	const result = unzipped.toString().split('\0')[1];
 	// result is the content of the file -> <content>
 	process.stdout.write(result);
 }
+
+function lsTree() {}
